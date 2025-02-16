fb06d5b05cda797d988d48685b44b07df025b10e
LuisPablo54 <luisirach54@gmail.com>
2025-02-15 18:39:03 -0600

    Cambio de estilo y cambios al menu

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function iniciarJuego(tipoJuego) {
-    alert('Iniciando juego de ' + tipoJuego + '!');
-    // Aquí se puede redirigir a la página correspondiente del juego
-    
-}
+    if (tipoJuego === 'suma') {
+        window.location.href = './suma/suma.html';
+    } 
+    else if (tipoJuego === 'resta') {
+        window.location.href = './resta/resta.html';
+    } 
+    else if (tipoJuego === 'multiplicacion') {
+        window.location.href = './multiplicacion/multiplicacion.html';
+    } 
+    else if (tipoJuego === 'division') {
+        window.location.href = './division/division.html';
+    }
+    else if (tipoJuego === 'fracciones') {
+        window.location.href = './fracciones/fracciones.html';
+    }
+    else {
+        alert('En desarrollo: ' + tipoJuego);
+    }
 
+
+}
